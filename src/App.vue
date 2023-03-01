09a0ed1d5e0fdaf3b2fe6c0d2b121da93d67aa3e
firttrule <vanezik23@mail.ru>
2023-03-01 18:40:11 +0300

    добавлена отметка выполненной задачи

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,35 @@
       </div>
     </form>
 
-    <div v-for="todo in todos" :key="todo.id" class="card mb-5">
+    <div
+      v-for="todo in todos"
+      :key="todo.id"
+      class="card mb-5"
+      :class="{ 'has-background-success-light': todo.done }"
+    >
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">{{ todo.content }}</div>
+            <div
+              class="column"
+              :class="{ 'has-text-success line-through': todo.done }"
+            >
+              {{ todo.content }}
+            </div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">&check;</button>
-              <button class="button is-danger ml-2">&cross;</button>
+              <button
+                @click="toggleDone(todo.id)"
+                class="button"
+                :class="todo.done ? 'is-success' : 'is-light'"
+              >
+                &check;
+              </button>
+              <button
+                @click="deleteTodo(todo.id)"
+                class="button is-danger ml-2"
+              >
+                &cross;
+              </button>
             </div>
           </div>
         </div>
@@ -43,16 +64,16 @@ import { v4 as uuidv4 } from "uuid";
 // todos
 
 const todos = ref([
-  //   {
-  //     id: "id1",
-  //     content: "hello guys!",
-  //     done: false,
-  //   },
-  //   {
-  //     id: "id2",
-  //     content: "check this thing!",
-  //     done: false,
-  //   },
+  // {
+  //   id: "id1",
+  //   content: "hello guys!",
+  //   done: false,
+  // },
+  // {
+  //   id: "id2",
+  //   content: "check this thing!",
+  //   done: true,
+  // },
 ]);
 
 // add todo
@@ -63,10 +84,21 @@ const addTodo = () => {
   const newTodo = {
     id: uuidv4(),
     content: newtodoContent.value,
-    done: "boolean",
+    done: false,
   };
   todos.value.unshift(newTodo);
   newtodoContent.value = "";
+};
+
+// delete todo
+const deleteTodo = (id) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
+
+// toggle done
+const toggleDone = (id) => {
+  const index = todos.value.findIndex((todo) => todo.id === id);
+  todos.value[index].done = !todos.value[index].done;
 };
 </script>
 
@@ -78,5 +110,9 @@ const addTodo = () => {
   max-width: 400px;
   padding: 20px;
   margin: 0 auto;
+}
+
+.line-through {
+  text-decoration: line-through;
 }
 </style>
